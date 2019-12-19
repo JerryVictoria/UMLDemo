e22d304bd0e1e15bb2e7e46e8e25471f3317139a
未至佳期 <37244260+JerryVictoria@users.noreply.github.com>
2019-12-19 16:35:51 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
-
+import "./components/util/checkoutside.js";
+import store from "./store.js";
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
 new Vue({
+    store,
     render: h => h(App)
 }).$mount("#app");
