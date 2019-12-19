5bf9d5cf6118073f8fd19a497f009769d3b5e667
未至佳期 <37244260+JerryVictoria@users.noreply.github.com>
2019-12-19 16:34:18 +0800

    Update CommonComponent.vue

--- src/components/CommonComponent.vue
+++ src/components/CommonComponent.vue
@@ -1,17 +1,15 @@
 <script>
 export default {
     name: "CommonComponent",
+    props: {
+        id: String,
+        type: String
+    },
     data() {
         return {
             width: "200px",
-            height: "150px"
+            height: "180px"
         };
-    },
-    created() {
-        document.body.addEventListener("click", this.hideInputAndSave);
-    },
-    destroyed() {
-        document.body.removeEventListener("click", this.hideInputAndSave);
     },
     methods: {
         setWidth(width) {
@@ -20,7 +18,35 @@ export default {
         setHeight(height) {
             this.height = height;
         },
-        hideInputAndSave() {}
+        hideInputAndSave() {
+            this.setEditStateFalse();
+        },
+        setEditStateFalse() {
+            this.$store.commit("setEditState", { editing: false });
+            this.$store.commit("setEditId", { id: "" });
+        },
+        setEditStateTrue() {
+            this.$store.commit("setEditState", { editing: true });
+        },
+        setEditingId() {
+            this.$store.commit("setEditId", { id: this.id });
+        },
+        commitWidth() {
+            this.$store.commit("modifyNode", {
+                nodeKey: "styles",
+                key: "width",
+                value: this.width,
+                id: this.$store.state.editingId
+            });
+        },
+        commitHeight() {
+            this.$store.commit("modifyNode", {
+                nodeKey: "styles",
+                key: "height",
+                value: this.height,
+                id: this.$store.state.editingId
+            });
+        }
     }
 };
 </script>
